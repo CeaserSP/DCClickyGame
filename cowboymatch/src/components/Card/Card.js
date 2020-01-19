99aca6daacce366e0c64d14dc24f766a631cc128
Sheldon Perkins <csperkins18@gmail.com>
2020-01-19 03:01:51 -0500

    Image size fix

--- cowboymatch/src/components/Card/Card.js
+++ cowboymatch/src/components/Card/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 const Card = props => (
     <div className="card" onClick={() => props.handlePicked(props.id, props.value)}>
         <div className="img-container">
-            <img alt={props.id} src={props.image} />
+            <img className="img-container-img" alt={props.id} src={props.image} />
         </div>
     </div>
 );
