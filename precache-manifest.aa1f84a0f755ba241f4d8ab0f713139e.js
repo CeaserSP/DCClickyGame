86a4ac4258aaf75077ae565f91672df7884434cc
Sheldon Perkins <csperkins18@gmail.com>
2020-01-19 03:31:20 -0500

    Updates

---
+++ precache-manifest.aa1f84a0f755ba241f4d8ab0f713139e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6335fa3b2c773af7402373fe95187d",
+    "url": "/DCClickyGame/index.html"
+  },
+  {
+    "revision": "a97ac974964ba8a944ec",
+    "url": "/DCClickyGame/static/css/main.61082b72.chunk.css"
+  },
+  {
+    "revision": "0806cf63a278aebbde30",
+    "url": "/DCClickyGame/static/js/2.e1d41bde.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/DCClickyGame/static/js/2.e1d41bde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a97ac974964ba8a944ec",
+    "url": "/DCClickyGame/static/js/main.92b593c8.chunk.js"
+  },
+  {
+    "revision": "e5add9122fbed1432e4e",
+    "url": "/DCClickyGame/static/js/runtime-main.5fa9ff27.js"
+  },
+  {
+    "revision": "1af2a7a1128793d931eaae7913546e51",
+    "url": "/DCClickyGame/static/media/quinn.1af2a7a1.jpg"
+  }
+]);
